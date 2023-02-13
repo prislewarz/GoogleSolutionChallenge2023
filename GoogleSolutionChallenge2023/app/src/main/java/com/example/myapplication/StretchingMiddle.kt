376fe7c3f692aee8af66cc57557208c5f5ed963c
MinCOnMinCon <dlalstjq612@gmail.com>
2023-02-13 22:14:27 +0900

    스트레칭 화면의 다음 버튼을 누르는데 15초에 텀 생김

--- GoogleSolutionChallenge2023/app/src/main/java/com/example/myapplication/StretchingMiddle.kt
+++ GoogleSolutionChallenge2023/app/src/main/java/com/example/myapplication/StretchingMiddle.kt
@@ -3,10 +3,15 @@ package com.example.myapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Looper
 import com.example.myapplication.databinding.StretchingMiddleBinding
+import java.util.*
+import kotlin.concurrent.schedule
+import kotlin.concurrent.timer
 
 class StretchingMiddle : AppCompatActivity() {
 
+    //page_num_check : 현재의 페이지 수(page_num)을 받고 거기에 알맞는 스트레칭 설명문을 리턴
     fun page_num_check(page_num : Int): String{
         when (page_num){
 
@@ -18,10 +23,10 @@ class StretchingMiddle : AppCompatActivity() {
             7 -> return getString(R.string.neckup_des)
             8 -> return getString(R.string.neckdown_des)
             9 -> return getString(R.string.neckleftright_des)
-
+            // 1~5까지 눈 스트레칭 / 7~9까지는 목스트레칭 / 각각의 가장 처음 스트레칭 관련 코드는 아래 작성
         }
 
-    return "ERROR"
+        return "ERROR"
     }
 
     private lateinit var binding : StretchingMiddleBinding
@@ -29,14 +34,28 @@ class StretchingMiddle : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = StretchingMiddleBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        var page_num = 0
+
+        //인텐드가 EyeStretchingStart에서 오면 eyespin1이 옴 => 첫번째 눈 스트레칭 화면 작동, 그리고 다음 스트레칭이 나오도록 page_num을 1로 변화
+        var page_num = 0 //나중에 합치기
         if(intent.hasExtra("eyespin1")){
             binding.stretchingDes.text = getString(R.string.eyespin1_des)
             page_num = 1
-        }
+            binding.nextButton.isEnabled = false
+            Timer().schedule(15000) {
+                runOnUiThread {
+                    binding.nextButton.isEnabled = true
+                }
+            }
+        }//인텐드가 NeckStretchingStart에서 오면 neckforehead가 옴 => 첫번째 목 스트레칭 화면 작동, 그리고 다음 스트레칭이 나오도록 page_num을 7로 변화
         else if(intent.hasExtra("neckforehead")){
             binding.stretchingDes.text = getString(R.string.neckforehead_des)
             page_num = 7
+            binding.nextButton.isEnabled = false
+            Timer().schedule(15000) {
+                runOnUiThread {
+                    binding.nextButton.isEnabled = true
+                }
+            }
         }
 
         binding.nextButton.setOnClickListener{
@@ -47,6 +66,13 @@ class StretchingMiddle : AppCompatActivity() {
             else{
                 binding.stretchingDes.text = page_num_check(page_num)
                 page_num++
+                binding.nextButton.isEnabled = false
+                Timer().schedule(15000) {
+                    runOnUiThread {
+                        binding.nextButton.isEnabled = true
+                    }
+                }
+
             }
         }
 
